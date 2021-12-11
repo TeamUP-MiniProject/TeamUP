6ae396216bb7c72e51502eccc0c025697d72301a
ts0ron <ts0.ron@gmail.com>
2021-12-11 20:26:01 +0200

    Starting redefine the structure of thr routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./router/admin');
+
 const app = express();
 
 // building a body parser for each request 
@@ -11,19 +13,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 /*
 app.get or app.post is used to handle the request get/post accordingly.
 */
-start_path = "/personal_page";
-app.use(start_path, (req,res,next) => { 
-    res.send('<form action="/" method="POST"><input type="text" name="private_name"><button type="submit">Add User Name</button></form> ');
-    // res.redirect("/hobbies");   redirects to a different path.
- });
+
 // app.use gets a function with args (req, res, next) and execute the middleware for the request
 // in-order
-app.use((req,res,next) => { 
-    console.log(req.body);
-    res.send('<h6>Hello from middleware 1</h6>');
-    next() // Allows the request to continue to the next middleware in line
-    
-});
 
+app.use(adminRoutes); // Using the routes defined in the routes/admin.js by order - the order matters
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-hobby', (req, res, next) => {
+    res.send('<form action="/group-it/add-hobby" method="POST"><input type="text"  name="hobby_name"> <button type="submit"></button></input> </form>');
+});
+
+router.post('/group-it/add-hoby', (req, res, next) => {
+    console.log(req.body);
+    res.send('<h1>Hobby Added</h1>');
+});
+
+module.exports = router;
