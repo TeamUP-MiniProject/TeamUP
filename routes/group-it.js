e9a2374db1bf91dafcedce0aea161400af5645f6
ts0ron <ts0.ron@gmail.com>
2021-12-11 21:15:31 +0200

    Updating group-it.js amd group-it-home.html

--- routes/group-it.js
+++ routes/group-it.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const express = require('express');
 const { builtinModules } = require('module');
 const router = express.Router();
 
+router.get('/register-user' , (req,res,next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'register-user.html'));
+});
 router.get('/', (req, res, next) => {
-    res.send("<h1>You have reached the main web-page of Group-IT!</h1>")
+    res.sendFile(path.join(__dirname, '../', 'views', 'group-it-home.html'));
 });
 
 module.exports = router;
